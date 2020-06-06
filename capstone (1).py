b99453336637da15713dc397ab511f4337a5fc5d
HARINIMURUGAN2000 <66518365+HARINIMURUGAN2000@users.noreply.github.com>
2020-06-06 21:50:13 +0530

    Add files via upload

---
+++ capstone (1).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# # jupter notebook for capstone project
+
+# import pandas as pd
+# 
+
+# In[2]:
+
+
+import numpy  as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
